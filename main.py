62193fb107f02b96938066d6d245db990436f882
Muntasir Sauvik <muntasir@sas030.cpdist.ca>
2020-10-16 08:59:55 -0600

    Tasks completed up until Insert Expressions

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+
+# Version Check
 print("sqlalchemy verion:"+ sqlalchemy.__version__  )
 
+# Connecting , import statement at line 2 "from sqlalchemy import create_engine" is also needed.
 engine = create_engine('sqlite:///:memory:', echo=True)
 
-metadata = MetaData()
-users = Table('users', metadata, Column('id', Integer, primary_key=True), Column('name', String), Column('fullname', String),)
-addresses = Table('addresses', metadata, Column('id', Integer, primary_key=True), Column('user_id', None, ForeignKey('users.id')),
-                  Column('email_address', String, nullable=False))
 
+# Define and Create Tables, import statement at line 3 "from sqlalchemy import Table, Column, Integer, String, MetaData,
+# ForeignKey" is also needed.
+metadata = MetaData()
+users = Table('users', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('name', String),
+        Column('fullname', String),)
+
+addresses = Table('addresses', metadata,
+            Column('id', Integer, primary_key=True),
+            Column('user_id', None, ForeignKey('users.id')),
+            Column('email_address', String, nullable=False))
+
+# tell the MetaData we’d actually like to create our selection of tables for real inside the SQLite database
 metadata.create_all(engine)
 
+# Insert Expressions
 ins = users.insert()
-
 print (str(ins))
-
 ins = users.insert().values(name='jack', fullname='Jack Jones')
 print (str(ins))
+
